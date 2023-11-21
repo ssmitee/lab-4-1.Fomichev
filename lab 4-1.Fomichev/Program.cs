b3b2dcb0702cd3e6deb42ef8b6328df89a9062c8
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-11-21 18:18:21 +0200

    Добавьте файлы проекта.

---
+++ lab 4-1.Fomichev/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = new int[11];
+Console.WriteLine("Введите значения массива:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Элемент {i + 1}: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < 0 && array[i] % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 3 == 0)
+    {
+        array[i] = sum;
+    }
+}
+Console.WriteLine("\nИзмененный массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
